178175745b86aa80f99fc8ed9e993fef5ea5c7e8
KotlinGeekDev <kotlingeek@protonmail.com>
2025-02-11 11:51:21 +0100

    Configure the default OkHttp engine with ping interval support.

--- kostr-core/src/commonJvmMain/kotlin/ktnostr/net/HttpClient.commonJvm.kt
+++ kostr-core/src/commonJvmMain/kotlin/ktnostr/net/HttpClient.commonJvm.kt
@@ -2,8 +2,13 @@ package ktnostr.net
 
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 internal actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(OkHttp) {
+    engine {
+        preconfigured = OkHttpClient.Builder().pingInterval(15, TimeUnit.SECONDS).build()
+    }
     config(this)
 
 }
