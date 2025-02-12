43c2d7a5e0805c6247f2daa4461d4a55d13371aa
KotlinGeekDev <kotlingeek@protonmail.com>
2025-02-12 12:13:19 +0100

    Disable publish at the module level.

--- kostr-core/build.gradle.kts
+++ kostr-core/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("com.android.library")
 
     kotlin("plugin.serialization")
-    `maven-publish`
+//    `maven-publish`
 }
 
 
@@ -26,7 +26,6 @@ kotlin {
 
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     compilerOptions {
-//        jvmToolchain(17)
         apiVersion.set(KotlinVersion.KOTLIN_1_8)
         languageVersion.set(KotlinVersion.KOTLIN_1_8)
     }
