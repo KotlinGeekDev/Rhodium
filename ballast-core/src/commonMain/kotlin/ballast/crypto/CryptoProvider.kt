9176597b88c7e468235021ffe25fdf8edaf18f6c
KotlinGeekDev <kotlingeek@protonmail.com>
2025-02-14 21:18:17 +0100

    Remove the files that should have been removed.

--- ballast-core/src/commonMain/kotlin/ballast/crypto/CryptoProvider.kt
+++
@@ -1,7 +0,0 @@
-package ballast.crypto
-
-import dev.whyoleg.cryptography.CryptographyProvider
-import dev.whyoleg.cryptography.random.CryptographyRandom
-
-expect fun SecureRandom(): CryptographyRandom
-expect fun getCryptoProvider(): CryptographyProvider
--- ballast-core/src/commonMain/kotlin/ballast/net/HttpClient.kt
+++
@@ -1,5 +0,0 @@
-package ballast.net
-
-import io.ktor.client.*
-
-internal expect fun httpClient(config: HttpClientConfig<*>.() -> Unit = {}): HttpClient
