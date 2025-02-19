6b174bfa2dc050df09df4b070dd043105ebc29cc
KotlinGeekDev <kotlingeek@protonmail.com>
2025-02-19 18:54:51 +0100

    Move to new version: v1.0-beta-14.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
     val isJitpack = System.getenv("JITPACK") == "true"
 
     group = "io.github.kotlingeekdev"
-    version = "1.0-beta-13"
+    version = "1.0-beta-14"
 
 
 //    val javadocJar = tasks.register<Jar>("javadocJar") {
