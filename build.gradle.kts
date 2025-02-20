78975c6eddee9ee9203e00a69e88a10aaf51d4aa
KotlinGeekDev <kotlingeek@protonmail.com>
2025-02-20 17:53:21 +0100

    Some fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
     val isJitpack = System.getenv("JITPACK") == "true"
 
     group = "io.github.kotlingeekdev"
-    version = "1.0-beta-15"
+    version = "1.0-beta-15-post-2"
 
 
 //    val javadocJar = tasks.register<Jar>("javadocJar") {
